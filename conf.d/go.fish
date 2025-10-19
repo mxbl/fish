b8f09bd21de99fe31eacb859c6fb7e211c5e7643
Maximilian Block <4mxblc@pm.me>
2025-10-19 08:44:07 +0200

    I don't know what these changes are supposed to accomplish but somebody told me to make them.

--- conf.d/go.fish
+++ conf.d/go.fish
@@ -1,2 +1,4 @@
 fish_add_path /usr/local/go/bin
 fish_add_path ~/go/bin
+
+set -x GOBIN $HOME/go/bin
--- functions/check_requirements.fish
+++ functions/check_requirements.fish
@@ -1,6 +1,12 @@
-function check_requirements -d "check_requirements <[cmd]>"
+function check_requirements --argument cmds -d "Check a list of commands to be available"
+	if test (count $cmds) -ne 1
+		echo "Usage: check_requirements <[cmds]>"
+		return 1
+	end
 	set -l __status 0
 	set -l requirements $argv
+
+	return 0
 
 	for cmd in $requirements
 		if not command -sq $cmd
