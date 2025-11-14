4cc3d7743096f56ba04047ec58953d802d3d81ad
Maximilian Block <4mxblc@pm.me>
2025-11-14 11:09:44 +0100

    Add pretty print function to go_setup
    NOTE: make pp global?

--- functions/go_setup.fish
+++ functions/go_setup.fish
@@ -1,19 +1,30 @@
 function go_setup --description 'Setup Go environment and install specified Go packages'
 	check_requirements go mise; or return 1
 
-	function info --description 'Print info message to stderr'
-		printf "[INFO] %s%s%s\n" (set_color 5fdfff) $argv (set_color normal) >/dev/stderr
+	function pp --argument type rest --description 'Pretty Print message to stderr'
+		switch $type
+			case info
+				set -f color 5fdfff
+			case ok
+				set -f color green
+			case err
+				set -f color red
+			case '*'
+				set -f color normal
+		end
+		printf "[%s] %s%s%s\n" (string upper $type) (set_color $color) $rest (set_color normal) >/dev/stderr
 	end
 
 	set -l go_packages                          \
 		golang.org/x/tools/cmd/goimports@latest \
 		mvdan.cc/gofumpt@latest                 \
 		github.com/segmentio/golines@latest     \
+		github.com/air-verse/air@latest
 
 	for pkg in $go_packages
-		info "Installing $pkg..."
+		pp info "Installing $pkg..."
 		go install $pkg
 	end
 
-	info "Go environment setup complete."
+	pp ok "Go environment setup complete."
 end
