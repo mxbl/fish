ec0df29cd31ed4a01f49d4f4f94635b8b1164593
Maximilian Block <4mxblc@pm.me>
2025-11-29 09:10:36 +0100

    Add ansible env vars

--- conf.d/ansible.fish
+++ conf.d/ansible.fish
@@ -1,3 +1,4 @@
 set -x ANSIBLE_CONFIG $HOME/dev/.ansible.cfg
 set -x ANSIBLE_INVENTORY ./hosts.yml
 set -x ANSIBLE_VAULT_PASSWORD_FILE $HOME/.ansible/vault-secret
+set -x ANSIBLE_ROLES_PATH $HOME/dev/ansible-roles
