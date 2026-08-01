cafaec5eaa0c83a8f40dc22655a4be2b2975f6b0
Maximilian Block <4mxblc@pm.me>
2026-08-01 20:50:26 +0200

    fix

--- config.fish
+++ config.fish
@@ -11,7 +11,6 @@ set -x EDITOR nvim
 # SSH agent socket discovery (when not on remote)
 # TODO: do we need this anyways?
 if not set -q SSH_CLIENT
-	echo $SSH_AUTH_SOCK
 	set -l base /run/user/(id -u)
 	set -l sockets \
 		$base/keyring/ssh \
